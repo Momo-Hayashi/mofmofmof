86166f62cfb341b4f1fc15e844f0e5bc4604247f
Momo Hayashi <14hb108e@al.rikkyo.ac.jp>
2021-06-21 11:43:12 +0900

    最寄り駅データが空の場合保存させないようにmodel,controllerを編集

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,39 +4,52 @@ class PropertiesController < ApplicationController
   def index
     @property = Property.all
   end
+
   def new
     @property = Property.new
     2.times { @property.closest_stations.build }
   end
+
   def confirm
     @property = Property.new(property_params)
-    # @closest_stations = @property.closest_stations(property_params)
-    # binding.irb
-    render :new if @property.invalid?
+    if @property.invalid?
+      2.times { @property.closest_stations.build }
+      render :new
+    end
   end
+
   def create
     @property = Property.new(property_params)
     if params[:back]
+      2.times { @property.closest_stations.build }
       render :new
     else
       if @property.save
         redirect_to properties_path, notice: "保存しました！"
       else
+        2.times { @property.closest_stations.build }
         render :new
       end
     end
   end
+
   def show
     @closest_stations = @property.closest_stations
   end
 
+  def edit
+    @property.closest_stations.build
+  end
+
   def update
+    @closest_stations = @property.closest_stations
     if @property.update(property_params)
       redirect_to properties_path, notice:"アップデートしました！"
     else
       render :edit
     end
   end
+
   def destroy
     @property.destroy
     redirect_to properties_path, notice: "削除しました！"
@@ -44,10 +57,23 @@ class PropertiesController < ApplicationController
 
   private
   def property_params
-  params.require(:property).permit(:name, :price, :address, :years_old, :note,
-    closest_stations: [:line, :staion, :distance, :property_id])
+    params.require(:property).permit(
+      :name,
+      :price,
+      :address,
+      :years_old,
+      :note,
+      closest_stations_attributes: [
+        :line,
+        :station,
+        :distance,
+        :property_id,
+        :id,
+        :_destroy,
+      ])
   end
+
   def set_property
-  @property = Property.find(params[:id])
+    @property = Property.find(params[:id])
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -3,6 +3,16 @@ class Property < ApplicationRecord
   validates :price, presence: true
   validates :address, presence: true
   validates :years_old, presence: true
-  has_many :closest_stations, dependent: :destroy
-  accepts_nested_attributes_for :closest_stations, reject_if: :all_blank
+  has_many :closest_stations, dependent: :destroy, inverse_of: :property
+  accepts_nested_attributes_for :closest_stations, allow_destroy: true, reject_if: :reject_both_blank
+
+  def reject_both_blank(attributes)
+    if attributes[:id]
+      attributes.merge!(_destroy: "1") if attributes[:line].blank? and attributes[:station].blank? and attributes[:distance].blank?
+      !attributes[:line].blank? and attributes[:station].blank? and attributes[:distance].blank?
+    else
+      attributes[:line].blank? and attributes[:station].blank? and attributes[:distance].blank?
+    end
+  end
+
 end
